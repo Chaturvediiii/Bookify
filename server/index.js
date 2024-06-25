858a2cc9267dc8d78d9d423c0152e36a35961831
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-06-25 13:08:33 +0530

    database created

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
 
-const app = express()
-const PORT = 3000
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000,()=>{
-    console.log(`Server running on ${PORT}`);
-})
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(3000, () => {
+  console.log(`Server running on ${PORT}`);
+});
