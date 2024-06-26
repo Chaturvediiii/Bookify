f1ab4be3798bec3f928dc61497d0d348fdbee0c5
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-06-26 17:43:06 +0530

    header profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import Books from './pages/Books';
-import Contact from './pages/Contact';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Books from "./pages/Books";
+import Contact from "./pages/Contact";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='signin' element={<SignIn/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/books' element={<Books/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/books" element={<Books />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
-  )
+  );
 }
-
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import {useSelector} from 'react-redux'
 
 export default function Header() {
+  const {currentUser} = useSelector(state=>state.user)
   return (
     <header className="bg-blue-950 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-2">
@@ -24,8 +26,13 @@ export default function Header() {
           <Link to="/contact">
             <li className="hidden sm:inline text-cream">Contact Us</li>
           </Link>
-          <Link to="/signin">
-            <li className=" text-cream">Sign In</li>
+          <Link to="/profile">
+          {currentUser ? (
+            <img src={currentUser.avatar} alt="profile" className="rounded-full w-7 h-7 object-cover"/>
+          ) :
+          <li className=" text-cream">Sign In</li>
+          }
+            
           </Link>
         </ul>
       </div>
