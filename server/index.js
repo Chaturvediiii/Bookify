a500aedb56265659781c43e193ac5a4a287a2258
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-06-28 18:01:08 +0530

    update user api

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
-import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -19,6 +19,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(3000, () => {
   console.log(`Server running on ${PORT}`);
